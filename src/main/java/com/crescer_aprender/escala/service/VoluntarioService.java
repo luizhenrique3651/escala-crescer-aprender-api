2685dbe8ec26e5ce790deee41c3997a92b987a23
luizhenrique3651 <41950243+luizhenrique3651@users.noreply.github.com>
2025-05-09 22:21:50 -0400

    VoluntarioService implementado

---
+++ src/main/java/com/crescer_aprender/escala/service/VoluntarioService.java
@@ -0,0 +1,44 @@
+package com.crescer_aprender.escala.service;
+
+import com.crescer_aprender.escala.entity.Voluntario;
+import com.crescer_aprender.escala.repository.VoluntarioRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class VoluntarioService {
+
+    private final VoluntarioRepository repository;
+
+    public VoluntarioService(VoluntarioRepository repository) {
+        this.repository = repository;
+    }
+
+    public Voluntario save(Voluntario voluntario) {
+        return repository.save(voluntario);
+    }
+
+    public List<Voluntario> loadAll() {
+        return repository.findAll();
+    }
+
+    public Optional<Voluntario> update(Long id, Voluntario voluntario) {
+        return repository.findById(id).map(v -> {
+            v.setNome(voluntario.getNome());
+            v.setEmail(voluntario.getEmail());
+            v.setSenha(voluntario.getSenha());
+            v.setDatasDisponiveis(voluntario.getDatasDisponiveis());
+            return repository.save(v);
+        });
+    }
+
+    public boolean delete(Long id) {
+        if (repository.existsById(id)) {
+            repository.deleteById(id);
+            return true;
+        }
+        return false;
+    }
+}
