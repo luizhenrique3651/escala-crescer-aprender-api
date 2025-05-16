1e281459941217d210c4a7c1d67062cb6f6635d7
luizhenrique3651 <41950243+luizhenrique3651@users.noreply.github.com>
2025-05-15 22:01:36 -0400

    VoluntarioService aprimorado com lançamento de exceções no save, e merge de informações no update

--- src/main/java/com/crescer_aprender/escala/service/VoluntarioService.java
+++ src/main/java/com/crescer_aprender/escala/service/VoluntarioService.java
@@ -1,10 +1,14 @@
 package com.crescer_aprender.escala.service;
 
 import com.crescer_aprender.escala.entity.Voluntario;
+import com.crescer_aprender.escala.exception.ConstantExceptionUtil;
+import com.crescer_aprender.escala.exception.InvalidVoluntarioDataException;
+import com.crescer_aprender.escala.exception.VoluntarioNotFoundException;
 import com.crescer_aprender.escala.repository.VoluntarioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
 
@@ -19,22 +23,42 @@ public class VoluntarioService {
     }
 
     public Voluntario save(Voluntario voluntario) {
+        if (voluntario.getNome() == null || voluntario.getNome().isEmpty()) {
+            throw new InvalidVoluntarioDataException(ConstantExceptionUtil.INVALID_VOLUNTARIO_NAME);
+        }
+        if (voluntario.getEmail() == null || !voluntario.getEmail().contains("@")) {
+            throw new InvalidVoluntarioDataException(ConstantExceptionUtil.INVALID_VOLUNTARIO_EMAIL);
+        }
         return repository.save(voluntario);
     }
+
 
     public Optional<List<Voluntario>> loadAll() {
         return Optional.of(repository.findAll());
     }
 
-    public Optional<Voluntario> update(Long id, Voluntario voluntario) {
-        return repository.findById(id).map(v -> {
-            v.setNome(voluntario.getNome());
-            v.setEmail(voluntario.getEmail());
-            v.setSenha(voluntario.getSenha());
-            v.setDatasDisponiveis(voluntario.getDatasDisponiveis());
-            return repository.save(v);
-        });
+    public Voluntario update(Long id, Voluntario voluntario) {
+        Voluntario oldVoluntario = repository.findById(id)
+                .orElseThrow(() -> new VoluntarioNotFoundException(id));
+
+        Optional.ofNullable(voluntario.getNome()).ifPresent(oldVoluntario::setNome);
+        Optional.ofNullable(voluntario.getEmail()).ifPresent(oldVoluntario::setEmail);
+        Optional.ofNullable(voluntario.getSenha()).ifPresent(oldVoluntario::setSenha);
+        Optional.ofNullable(voluntario.getDatasDisponiveis())
+                .ifPresent(dates -> mergeDatasDisponiveis(oldVoluntario, dates));
+
+        return repository.save(oldVoluntario);
     }
+
+    private void mergeDatasDisponiveis(Voluntario voluntario, List<LocalDate> novasDatas) {
+        List<LocalDate> datasAtuais = voluntario.getDatasDisponiveis();
+
+        datasAtuais.removeIf(date -> !novasDatas.contains(date));
+        novasDatas.stream()
+                .filter(date -> !datasAtuais.contains(date))
+                .forEach(datasAtuais::add);
+    }
+
 
     public boolean delete(Long id) {
         if (repository.existsById(id)) {
