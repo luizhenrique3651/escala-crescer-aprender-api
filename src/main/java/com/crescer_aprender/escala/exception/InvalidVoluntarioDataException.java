449986bf6ab36c7f418a5b3e3041073c67b850f3
luizhenrique3651 <41950243+luizhenrique3651@users.noreply.github.com>
2025-05-15 22:00:49 -0400

    Novas exceptions adicionadas

---
+++ src/main/java/com/crescer_aprender/escala/exception/InvalidVoluntarioDataException.java
@@ -0,0 +1,7 @@
+package com.crescer_aprender.escala.exception;
+
+public class InvalidVoluntarioDataException extends RuntimeException {
+    public InvalidVoluntarioDataException(String message) {
+        super(message);
+    }
+}
