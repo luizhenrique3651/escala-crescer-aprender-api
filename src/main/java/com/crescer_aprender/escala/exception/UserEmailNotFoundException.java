56f765e4533bcc1034af90e0c0f5aaf0bdedc4e6
luizhenrique3651 <41950243+luizhenrique3651@users.noreply.github.com>
2026-01-18 19:49:42 -0400

    fix: corrigida nomenclatura de login para ficar de acordo com objeto real de usuário no banco

--- src/main/java/com/crescer_aprender/escala/exception/UserEmailNotFoundException.java
+++ src/main/java/com/crescer_aprender/escala/exception/UserEmailNotFoundException.java
@@ -3,8 +3,8 @@ package com.crescer_aprender.escala.exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.server.ResponseStatusException;
 
-public class UsernameNotFoundException extends ResponseStatusException {
-    public UsernameNotFoundException(String username) {
+public class UserEmailNotFoundException extends ResponseStatusException {
+    public UserEmailNotFoundException(String username) {
         super(HttpStatus.NOT_FOUND, "Usuário com email " + username + " não encontrado.");
     }
 }
